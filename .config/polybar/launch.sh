b64beb7b1d91857e84609e11679b51443d81fe6d
Nick Gilbert <nickgilbert2@gmail.com>
2017-11-24 16:05:23 -0800

    Split i3 workspaces into own bars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar example &
+polybar i3 &
+polybar main &
 
 
 echo "Bars launched..."
