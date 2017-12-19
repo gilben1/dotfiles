3614de10f178911c897fbcbe3d0b69ff2ce185b7
Nick Gilbert <nickgilbert2@gmail.com>
2017-12-19 08:52:06 -0800

    Properly set editor as vim and not 'visual' d'oh

--- .bashrc
+++ .bashrc
@@ -160,7 +160,7 @@ export LANGUAGE=en_US.UTF-8
 
 
 export VISUAL=vim
-export EDITOR="$VISUAL"
+export EDITOR=$VISUAL
 
 export CUPS_SERVER=print.cecs.pdx.edu:631
 export CUPS_USER=nickg
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ cat ~/ascii_art/affinitext7.ascii
 set -x PATH $PATH $HOME/.bin/scripts
 
 set -x VISUAL vim
-set -x EDITOR visual
+set -x EDITOR vim
 set -x CUPS_SERVER print.cecs.pdx.edu:631
 set -x CUPS_USER nickg
 
